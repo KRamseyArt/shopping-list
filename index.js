a96baf52cea6cdecdc0c735efeb8bc1000d5d0c5
Kalin Ramsey <kalinramsey@gmail.com>
2020-03-02 09:15:55 -0700

    added delete and toggle checks

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ function handleShoppingList(){
 
         addNewItem(getUserInput());
     });
+
+    $(handleToggles);
+    $(handleDeletes);
 }
 
 function getUserInput(){
@@ -21,10 +24,44 @@ function getUserInput(){
 
 function addNewItem(item){
     if(!item){
-        alert("please input an object to add to shopping list.");
+        alert("please type an item to be added to shopping list.");
     } else {
         alert(item + " has been added!");
+
+        let newHTML = "<li><span class=\"shopping-item\">" + item + "</span><div class=\"shopping-item-controls\"><button class=\"shopping-item-toggle\"><span class=\"button-label\">check</span></button><button class=\"shopping-item-delete\"><span class=\"button-label\">delete</span></button></div></li>";
+
+        $('.shopping-list').prepend(newHTML);
+        $('#shopping-list-entry').val('');
     }
+}
+
+function handleDeletes(){
+    $('.shopping-item-delete').on('click', event =>{
+        event.preventDefault();
+
+        let deletedItem = $(event.currentTarget).parentsUntil('.shopping-list');
+        alert("Deleted: " + deletedItem.find('.shopping-item').text());
+
+        $(deletedItem).remove();
+    })
+}
+
+function handleToggles(){
+    $('.shopping-item-toggle').on('click', event =>{
+        event.preventDefault();
+
+        let toggledItem = $(event.currentTarget).parentsUntil('.shopping-list');
+        
+        if (toggledItem.first().hasClass('.shopping-item__checked')){
+            toggledItem.first().removeClass('shopping-item__checked');
+            console.log("Unchecked: " + toggledItem.find('.shopping-item').text());
+        } else {
+            toggledItem.first().addClass('shopping-item__checked');
+            console.log("Checked: " + toggledItem.find('.shopping-item').text());
+        }
+        
+        
+    })
 }
 
 $(handleShoppingList);
